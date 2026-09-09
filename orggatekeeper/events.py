e57927930c16d34ebb6dcc5c414b7b0d6d11b706
Andreas Kring <andreas@magenta-aps.dk>
2026-09-09 08:43:28 +0200

    feat!: replace AMQP system with MOs new event system
    BREAKING CHANGE:
    
    The AMQP system has been replaced with MOs new event system which causes
    a breaking change in the code.

--- orggatekeeper/events.py
+++ orggatekeeper/events.py
@@ -1,71 +1,88 @@
 # SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
 
+from uuid import UUID
+
 import structlog
+from fastapi import APIRouter
+from fastramqpi.events import Event
 from fastramqpi.ramqp.depends import Context
-from fastramqpi.ramqp.depends import RateLimit
-from fastramqpi.ramqp.mo import MORouter
-from fastramqpi.ramqp.mo import PayloadUUID
 
 from . import calculate
 
-router = MORouter()
+events_router = APIRouter()
 
-logger = structlog.get_logger()
+logger = structlog.stdlib.get_logger()
 
 
-@router.register("org_unit")
-async def org_unit_handler(context: Context, uuid: PayloadUUID, _: RateLimit) -> None:
+async def org_unit_handler(context: Context, org_unit_uuid: UUID) -> None:
     """Callback to check org_unit_hierarchy.
 
     Listens to changes on org_units and it-accounts on org_units.
     """
 
-    logger.info("Changes to org_unit or its it-accounts", org_unit=uuid)
-    await calculate.update_line_management(**context, uuid=uuid)
+    logger.info("Changes to org_unit or its it-accounts", org_unit=org_unit_uuid)
+    await calculate.update_line_management(**context, uuid=org_unit_uuid)
 
 
-@router.register("ituser")
-async def ituser_callback(context: Context, payload: PayloadUUID, _: RateLimit) -> None:
+async def ituser_handler(context: Context, ituser_uuid: UUID) -> None:
     """Callback to check org_unit_hierarchy on changes to it-users."""
     try:
         org_units = await calculate.get_orgunit_from_ituser(
-            context["legacy_graphql_session"], payload
+            context["legacy_graphql_session"], ituser_uuid
         )
     except ValueError:
-        logger.debug("ITUser not found", payload=payload)
+        logger.debug("ITUser not found", ituser=ituser_uuid)
         return
     logger.info("Changes to it-user. Checking org_units", org_unit=org_units)
     await calculate.update(context, org_units)
 
 
-@router.register("association")
-async def association_callback(
-    context: Context, payload: PayloadUUID, _: RateLimit
-) -> None:
+async def association_handler(context: Context, association_uuid: UUID) -> None:
     """Callback to check org_unit_hierarchy on changes to associations."""
     try:
         org_units = await calculate.get_orgunit_from_association(
-            context["legacy_graphql_session"], payload
+            context["legacy_graphql_session"], association_uuid
         )
     except ValueError:
-        logger.debug("Association not found", payload=payload)
+        logger.debug("Association not found", association=association_uuid)
         return
     logger.info("Changes to association. Checking org_units", org_unit=org_units)
     await calculate.update(context, org_units)
 
 
-@router.register("engagement")
-async def engagement_callback(
-    context: Context, payload: PayloadUUID, _: RateLimit
-) -> None:
+async def engagement_handler(context: Context, engagement_uuid: UUID) -> None:
     """Callback to check org_unit_hierarchy on changes to engagements."""
     try:
         org_units = await calculate.get_orgunit_from_engagement(
-            context["legacy_graphql_session"], payload
+            context["legacy_graphql_session"], engagement_uuid
         )
     except ValueError:
-        logger.debug("Engagement not found", payload=payload)
+        logger.debug("Engagement not found", engagement=engagement_uuid)
         return
     logger.info("Changes to engagement. Checking org_units", org_unit=org_units)
     await calculate.update(context, org_units)
+
+
+@events_router.post("/events/mo/org_unit")
+async def _org_unit_event(context: Context, event: Event[UUID]) -> None:
+    logger.info("Received org_unit event", org_unit_event=event.dict())
+    await org_unit_handler(context, event.subject)
+
+
+@events_router.post("/events/mo/ituser")
+async def _ituser_event(context: Context, event: Event[UUID]) -> None:
+    logger.info("Received ituser event", ituser_event=event.dict())
+    await ituser_handler(context, event.subject)
+
+
+@events_router.post("/events/mo/association")
+async def _association_event(context: Context, event: Event[UUID]) -> None:
+    logger.info("Received association event", association_event=event.dict())
+    await association_handler(context, event.subject)
+
+
+@events_router.post("/events/mo/engagement")
+async def _engagement_event(context: Context, event: Event[UUID]) -> None:
+    logger.info("Received engagement event", engagement_event=event.dict())
+    await engagement_handler(context, event.subject)
